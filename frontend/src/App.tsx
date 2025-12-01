36297f3613005f198c70b16c6406a32739b342da
krish12345q89 <krish.arora@qualyval.com>
2025-12-02 03:26:56 +0530

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// React import not required with the new JSX transform
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
